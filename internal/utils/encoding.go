ec66d43b08a10581b568415db840e94b0a43ec22
William Collins <26700607+wcollins@users.noreply.github.com>
2026-05-29 11:28:13 -0400

    fix: return error from UnmarshalData instead of fatal

--- internal/utils/encoding.go
+++ internal/utils/encoding.go
@@ -6,6 +6,7 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/itential/ipctl/internal/logging"
 	"gopkg.in/yaml.v2"
@@ -22,16 +23,16 @@ func ToMap(in any, out any) error {
 	return json.Unmarshal(b, &out)
 }
 
-// UnmarshalData will attempt to to unmarshal a byte array into an object.  It
+// UnmarshalData will attempt to unmarshal a byte array into an object.  It
 // will first attempt to unmarshal the byte array as JSON.  If that fails, it
-// will attempt to unmarshal the data as YAML.
-func UnmarshalData(data []byte, ptr any) {
-	// FIXME (privateip) This function should be refactored to return an error
-	// instead of simply logging a fatal error.
+// will fall back to unmarshalling the data as YAML.  An error is returned only
+// when the data cannot be decoded as either format.
+func UnmarshalData(data []byte, ptr any) error {
 	if err := json.Unmarshal(data, ptr); err != nil {
-		logging.Error(err, "failed to unmarshal json data")
-		if err = yaml.Unmarshal(data, ptr); err != nil {
-			logging.Fatal(err, "failed to unmarshal data")
+		logging.Debug("failed to unmarshal data as json, falling back to yaml: %s", err)
+		if yamlErr := yaml.Unmarshal(data, ptr); yamlErr != nil {
+			return fmt.Errorf("failed to unmarshal data as json or yaml: %w", yamlErr)
 		}
 	}
+	return nil
 }
